af1781ee4daa2ff5d571384e4973e7119a48bc3e
andresds <andre_smiths@outlook.com>
2022-08-08 10:09:52 -0300

    test exaple with "@testing-library/react-hooks"

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import { Login } from "./pages/Login";
+import { useAuthUser } from "./context/AuthUser";
+import { AuthUserProvider } from "./context/AuthUser";
+import userEvent from "@testing-library/user-event";
+
+jest.mock("react-router-dom", () => ({
+  useNavigate: () => jest.fn(),
+}));
+
+jest.mock("./context/AuthUser", () => ({
+  useAuthUser: () => ({
+    user: null,
+    login: mockLogin,
+    logout: mockLogout,
+  }),
+}));
+
+const mockLogin = jest.fn();
+const mockLogout = jest.fn();
+
+describe("Login", () => {
+  afterEach(cleanup);
+
+  test("returns user undefined", () => {
+    render(<Login />, { wrapper: AuthUserProvider });
+    const { result } = renderHook(() => useAuthUser());
+    expect(result.current.user).toBeFalsy();
+  });
+
+  test("returns logged in user", async () => {
+    const { result } = renderHook(() => useAuthUser());
+
+    act(() => {
+      result.current.login();
+      result.current.user = {
+        username: "andre",
+        password: "123156",
+      }
+    });
+
+    expect(result.current.login).toBeCalledTimes(1);
+
+    expect(result.current.user).toEqual({
+      username: "andre",
+      password: "123156",
+    })
+  });
 });
